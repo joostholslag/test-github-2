33422ea75d51d4fec59fd3547bc3146c84926db5
Joost Holslag <joost@Joosts-iMac.fritz.box>
2015-07-30 11:20:19 +0200

    Initial Commit

---
+++ Test github2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test github2
+//
+//  Created by Joost Holslag on 30/07/15.
+//  Copyright (c) 2015 Joost Holslag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test github2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test github2
+//
+//  Created by Joost Holslag on 30/07/15.
+//  Copyright (c) 2015 Joost Holslag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
