0c7c07119bbdd552de326a980de0dd98624c97e5
Joost Holslag <joost@Joosts-iMac.fritz.box>
2015-07-30 11:24:10 +0200

    First commit

--- Test github2/ViewController.m
+++ Test github2/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+        NSLog(@"Can't wait for this to upload to GitHub");
 }
 
 - (void)didReceiveMemoryWarning {
